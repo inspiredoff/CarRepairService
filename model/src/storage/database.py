080d0d43d34f2985d69ce97bdce068a20d4b4dea
inspiredoff <Vindsf@gmail.com>
2024-09-06 00:02:25 +0300

    dev async_engine

--- model/src/storage/database.py
+++ model/src/storage/database.py
@@ -6,6 +6,8 @@ from sqlalchemy.ext.asyncio import (
 
 from sqlalchemy.orm import DeclarativeBase
 
+async_engine = create_async_engine(url=dsn, echo=True)
+async_session_factory = async_sessionmaker(async_engine)
 
 class Base(AsyncAttrs, DeclarativeBase):
     pass
