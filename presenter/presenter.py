bedffd2064b282a43d175d3af544128c67a35e21
inspiredoff <Vindsf@gmail.com>
2024-09-15 01:48:03 +0300

    fix presenter

--- presenter/presenter.py
+++ presenter/presenter.py
@@ -10,32 +10,32 @@ class Presenter:
 
     #Client
     async def get_all_client(self):
-        clients = await self.services.get_all_client()
+        await self.services.get_all_client()
 
     async def get_client_by_id(self, id: int):
-        client = await self.services.get_client_by_id(id)
+        await self.services.get_client_by_id(id)
 
     async def get_client_by_first_name(self, first_name: str):
-        client = await self.services.get_client_by_first_name(first_name)
+        await self.services.get_client_by_first_name(first_name)
 
     async def get_client_by_family_name(self, family_name: str):
-        client = await self.services.get_client_by_family_name(family_name)
+        await self.services.get_client_by_family_name(family_name)
 
     async def get_client_by_last_name(self, last_name: str):
-        client = await self.services.get_client_by_last_name(last_name)
+        await self.services.get_client_by_last_name(last_name)
 
     #Car
     async def get_all_car(self):
-        cars = await self.services.get_all_car()
+        await self.services.get_all_car()
 
     async def get_car_by_id(self, id: int):
-        car = await self.services.get_car_by_id(id)
+        await self.services.get_car_by_id(id)
 
     async def get_car_by_brand(self, brand: str):
-        car = await self.services.get_car_by_brand(brand)
+        await self.services.get_car_by_brand(brand)
 
     async def get_car_by_model(self, model: str):
-        car = await self.services.get_car_by_model(model)
+        await self.services.get_car_by_model(model)
 
     #Repair
     async def get_all_repair(self):
